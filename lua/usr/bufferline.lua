7e8565a16f4a52c0f77a97a9236bd51a1f916966
photonCarry <yyisgod@yeah.net>
2023-05-15 13:20:54 +0800

    调整buffline tab展示时跳过File Tree

--- lua/usr/bufferline.lua
+++ lua/usr/bufferline.lua
@@ -1,11 +1,22 @@
 require('bufferline').setup {
   options = {
     numbers = function(opts)
+      -- return string.format('%s|%s', opts.id, opts.raise(opts.ordinal))
       return string.format('%s', opts.ordinal)
     end,
-    diagnostics = false,
+    diagnostics = "coc",
     show_buffer_close_icons = false,
     show_close_icon = false,
     max_name_length = 80,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = function()
+          return vim.fn.getcwd()
+        end,
+        highlight = "Directory",
+        separator = true 
+      }
+    },
   },
 }
